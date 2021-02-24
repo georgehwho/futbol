4820d4c53665d09cf1e8e364a382f209e540635b
Olivia Charles <ocharles92@gmail.com>
2021-02-23 21:51:19 -0500

    Adjusted spacing for the initialize.

--- lib/team.rb
+++ lib/team.rb
@@ -5,12 +5,13 @@ class Team
               :abbreviation,
               :stadium,
               :link
+
   def initialize(data)
-    @team_id     = data[:team_id]
-    @franchiseId = data[:franchiseId]
-    @teamName    = data[:teamName]
-    @abbreviation = data[:abbreviation]
-    @stadium     = data[:stadium]
-    @link        = data[:link]
+    @team_id      = data[:team_id].to_i
+    @franchiseId  = data[:franchiseId].to_i
+    @teamName     = data[:teamName].to_s
+    @abbreviation = data[:abbreviation].to_s
+    @stadium      = data[:stadium].to_s
+    @link         = data[:link].to_s
   end
 end
