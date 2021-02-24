11f508dfa9022c179eccaac91609e4163c125bbf
George Ho <george.h.w.ho@gmail.com>
2021-02-23 21:16:23 -0500

    Add game stats integration

--- lib/stat_tracker.rb
+++ lib/stat_tracker.rb
@@ -1,10 +1,20 @@
+require_relative 'game_stats'
+
 class StatTracker
+  attr_reader :game_stats
 
   def self.from_csv(locations)
   	new(locations)
-  end 
+  end
 
-  def initialize(locations_hash)
-  end 
+  def initialize(locations)
+    @game_stats = GameStats.new(locations[:games])
+    # @game_teams = load_csv(locations[:game_teams], GameTeam)
+    # @teams = load_csv(locations[:teams], Team)
+  end
 
-end 
+  def highest_total_score
+    game_stats.highest_total_score
+  end
+
+end
