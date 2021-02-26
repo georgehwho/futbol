8ede17299c37f6cc6b1630c1e2d4fab454c7d6bf
George Ho <george.h.w.ho@gmail.com>
2021-02-26 09:47:53 -0500

    Update test to use truncated files

--- test/stat_tracker_test.rb
+++ test/stat_tracker_test.rb
@@ -1,13 +1,12 @@
 require "./test/test_helper"
-require './lib/stat_tracker'
 
 class StatTrackerTest < Minitest::Test
 
 	def setup
 		@locations = {
-		  games: './data/games.csv',
+		  games: './test/truncated_csv/games_truncated.csv',
 		  teams: './data/teams.csv',
-		  game_teams: './data/game_teams.csv'
+		  game_teams: './test/truncated_csv/game_teams_truncated.csv'
 		}
 		@stat_tracker = StatTracker.from_csv(@locations)
 	end
@@ -18,6 +17,12 @@ class StatTrackerTest < Minitest::Test
 	end
 
 	def test_it_can_find_the_highest_total_score
-		assert_equal 11, @stat_tracker.highest_total_score
+		# skip
+		assert_equal 6, @stat_tracker.highest_total_score
+	end
+
+	def test_it_can_find_the_lowest_total_score
+		# skip
+		assert_equal 1, @stat_tracker.lowest_total_score
 	end
 end
