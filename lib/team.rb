b89235f28ee6d949c9d79a264c9a02cb414081a8
George Ho <george.h.w.ho@gmail.com>
2021-02-26 10:07:01 -0500

    Fix franchise id and team name to fit ruby syntax

--- lib/team.rb
+++ lib/team.rb
@@ -1,15 +1,15 @@
 class Team
   attr_reader :team_id,
-              :franchiseId,
-              :teamName,
+              :franchise_id,
+              :team_name,
               :abbreviation,
               :stadium,
               :link
 
   def initialize(data)
     @team_id      = data[:team_id].to_i
-    @franchiseId  = data[:franchiseId].to_i
-    @teamName     = data[:teamName]
+    @franchise_id = data[:franchiseid].to_i
+    @team_name    = data[:teamname]
     @abbreviation = data[:abbreviation]
     @stadium      = data[:stadium]
     @link         = data[:link]
--- test/team_test.rb
+++ test/team_test.rb
@@ -5,8 +5,8 @@ class TeamTest < Minitest::Test
   def setup
     @arsenal = Team.new({
                         team_id: 1,
-                        franchiseId: 11,
-                        teamName: "Arsenal",
+                        franchiseid: 11,
+                        teamname: "Arsenal",
                         abbreviation: "UK",
                         stadium: "Stadium in the UK",
                         link: "/api/v1/teams/1"
@@ -16,8 +16,8 @@ class TeamTest < Minitest::Test
   def test_it_exists_and_has_attributes
     assert_instance_of Team, @arsenal
     assert_equal 1, @arsenal.team_id
-    assert_equal 11, @arsenal.franchiseId
-    assert_equal "Arsenal", @arsenal.teamName
+    assert_equal 11, @arsenal.franchise_id
+    assert_equal "Arsenal", @arsenal.team_name
     assert_equal "Stadium in the UK", @arsenal.stadium
     assert_equal "/api/v1/teams/1", @arsenal.link
   end
