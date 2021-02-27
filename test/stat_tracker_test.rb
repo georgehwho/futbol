ba6721fbdd5165e02f7d0a06cc38cf88098f2013
Olivia Charles <ocharles92@gmail.com>
2021-02-27 11:34:04 -0500

    Wrote test for stat tracker to count teams and info hash

--- test/stat_tracker_test.rb
+++ test/stat_tracker_test.rb
@@ -25,4 +25,19 @@ class StatTrackerTest < Minitest::Test
 		# skip
 		assert_equal 1, @stat_tracker.lowest_total_score
 	end
+
+	def test_it_can_count_teams
+		assert_equal 32, @stat_tracker.count_of_teams
+	end
+
+	def test_it_can_get_team_info
+	expected = {
+		"team_id" => "18",
+		"franchise_id" => "34",
+		"team_name" => "Minnesota United FC",
+		"abbreviation" => "MIN",
+		"link" => "/api/v1/teams/18"
+	}
+	assert_equal expected, @stat_tracker.team_info("18")
+	end
 end
