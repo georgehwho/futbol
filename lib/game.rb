87f70f90ef4b622a417709c2d4f66872811da095
George Ho <george.h.w.ho@gmail.com>
2021-02-27 15:13:46 -0500

    Update test to accomodate string IDs

--- lib/game.rb
+++ lib/game.rb
@@ -12,11 +12,11 @@ class Game
 
   def initialize(data)
     @game_id      = data[:game_id]
-    @season       = data[:season].to_i
+    @season       = data[:season]
     @type         = data[:type]
     @date_time    = data[:date_time]
-    @away_team_id = data[:away_team_id].to_i
-    @home_team_id = data[:home_team_id].to_i
+    @away_team_id = data[:away_team_id]
+    @home_team_id = data[:home_team_id]
     @away_goals   = data[:away_goals].to_i
     @home_goals   = data[:home_goals].to_i
     @venue        = data[:venue]
