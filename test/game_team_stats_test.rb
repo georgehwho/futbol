a5d10866d391b02235d253054833f6ca5c840ec2
Carlyle Douglas <cdouglascorp@gmail.com>
2021-02-27 18:13:44 -0700

    Corrected variable name

--- test/game_team_stats_test.rb
+++ test/game_team_stats_test.rb
@@ -1,7 +1,7 @@
 require "./test/test_helper"
 
 class GameTeamStatsTest < Minitest::Test
-  attr_reader :game_teams_stats,
+  attr_reader :game_team_stats,
               :stat_tracker
 
   def setup
@@ -11,16 +11,16 @@ class GameTeamStatsTest < Minitest::Test
       game_teams: './test/truncated_csv/game_teams_truncated.csv'
     }
     @stat_tracker = StatTracker.new(@locations)
-    @game_teams_stats = stat_tracker.game_team_stats
+    @game_team_stats = stat_tracker.game_team_stats
   end
 
   def test_it_exists
-    assert_instance_of GameTeamStats, game_teams_stats
+    assert_instance_of GameTeamStats, game_team_stats
   end
 
   def test_it_has_readable_attributes
-    assert_equal stat_tracker, game_teams_stats.stat_tracker
-    assert_equal 50, game_teams_stats.game_teams.size
+    assert_equal stat_tracker, game_team_stats.stat_tracker
+    assert_equal 50, game_team_stats.game_teams.size
   end
 
   def test_it_can_load_from_csv
@@ -30,6 +30,10 @@ class GameTeamStatsTest < Minitest::Test
 
     first_game_teams = GameTeam.new(first_row)
 
-    assert_instance_of Array, game_teams_stats.game_teams
+    assert_instance_of Array, game_team_stats.game_teams
+  end
+
+  def test_it_can_count_teams
+    assert_equal 8, game_team_stats.count_of_teams
   end
 end
