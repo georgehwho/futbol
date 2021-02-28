42300417e5ac7619f5bf7c940ba25ca55678e7fb
Olivia Charles <ocharles92@gmail.com>
2021-02-28 12:29:51 -0500

    Merged files with Dougie's method.

--- lib/game_team_stats.rb
+++ lib/game_team_stats.rb
@@ -17,26 +17,6 @@ class GameTeamStats
     @game_teams = load_csv(file_path, GameTeam)
   end
 
-
-  def array_by_team_id(id)
-    all_games =[]
-    @game_teams.each do |game_team|
-       if game_team.team_id == id
-         all_games << game_team
-      end
-    end
-    all_games
-  end
-
-  def number_of_wins(id)
-    array_by_team_id(id).find_all do  |game_team|
-      game_team.result == "WIN"
-    end.count
-  end
-
-  def average_win_percentage(id)
-  (number_of_wins(id) /array_by_team_id(id).count.to_f).round(2)
-
   def count_of_teams
     game_teams.uniq do |team|
       team.team_id
@@ -58,6 +38,25 @@ class GameTeamStats
     end
     best_offense_team_id = team_with_best_offense[0]
     stat_tracker.team_stats.find_by_id(best_offense_team_id).team_name
+  end
 
+  def array_by_team_id(id)
+    all_games =[]
+    @game_teams.each do |game_team|
+      if game_team.team_id == id
+         all_games << game_team
+      end
+    end
+    all_games
+  end
+
+  def number_of_wins(id)
+    array_by_team_id(id).find_all do  |game_team|
+      game_team.result == "WIN"
+    end.size
+  end
+
+  def average_win_percentage(id)
+  (number_of_wins(id) /array_by_team_id(id).size.to_f).round(2)
   end
 end
