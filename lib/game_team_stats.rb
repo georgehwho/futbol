3dcb3daf9361271d0356eb1ccdfcdfb10344b4f4
Carlyle Douglas <cdouglascorp@gmail.com>
2021-03-01 14:23:20 -0700

    Add highest_scoring_home_team method

--- lib/game_team_stats.rb
+++ lib/game_team_stats.rb
@@ -85,6 +85,19 @@ class GameTeamStats
 
     highest_average_goal_team_id = hash_average_goals.max_by { |k,v| v }[0]
     stat_tracker.team_stats.find_by_id(highest_average_goal_team_id).team_name
+  end
+
+  def highest_scoring_home_team
+    home_gt = game_teams.find_all { |gt| gt.hoa == 'home'}
+    gt_hash = group_game_teams_by_team_id(home_gt)
+
+    hash_average_goals = {}
+    gt_hash.each do |t_id, gt|
+      hash_average_goals[t_id] = average_goals(gt)
+    end
+
+    highest_average_goal_team_id = hash_average_goals.max_by { |k,v| v }[0]
+    stat_tracker.team_stats.find_by_id(highest_average_goal_team_id).team_name
   end 
 
   def find_team_win_percentage(list_of_game_teams = game_teams, id)
@@ -112,23 +125,23 @@ class GameTeamStats
     hash_game_games_in_season[winningest_team_id][0].head_coach
   end
 
-  def worst_coach(season)
-    games_in_a_season = stat_tracker.game_stats.game_ids_by_season(season)
+  # def worst_coach(season)
+  #   games_in_a_season = stat_tracker.game_stats.game_ids_by_season(season)
 
-    game_teams_in_a_season = game_teams.find_all do |game_team|
-      games_in_a_season.include?(game_team.game_id)
-    end
+  #   game_teams_in_a_season = game_teams.find_all do |game_team|
+  #     games_in_a_season.include?(game_team.game_id)
+  #   end
 
-    hash_game_games_in_season = group_game_teams_by_team_id(game_teams_in_a_season)
+  #   hash_game_games_in_season = group_game_teams_by_team_id(game_teams_in_a_season)
 
-    team_wins = {}
-    hash_game_games_in_season.each do |team_id, list_of_game_teams|
-      team_wins[team_id] = find_team_win_percentage(list_of_game_teams, team_id)
-    end
+  #   team_wins = {}
+  #   hash_game_games_in_season.each do |team_id, list_of_game_teams|
+  #     team_wins[team_id] = find_team_win_percentage(list_of_game_teams, team_id)
+  #   end
 
-    winningest_team_id = team_wins.min_by { |key,value| value }[0]
+  #   winningest_team_id = team_wins.min_by { |key,value| value }[0]
 
-    hash_game_games_in_season[winningest_team_id]
-    # require 'pry'; binding.pry
-  end
+  #   hash_game_games_in_season[winningest_team_id]
+  #   # require 'pry'; binding.pry
+  # end
 end
